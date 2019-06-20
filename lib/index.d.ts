11a7f605993d85571d59f2593e59dd1b68d92482
Julien Sarazin <julien.sarazin@me.com>
2019-06-20 14:58:13 +0200

    finalizing deployment

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './constants';
-export * from './interfaces';
-export * from './nest-kong.module';
-export * from './nest-kong.service';
-//# sourceMappingURL=index.d.ts.map
--- lib/nest-kong.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-import { KongModuleConfiguration } from './interfaces';
-export declare class NestKongModule {
-    static init(options: KongModuleConfiguration): DynamicModule;
-}
-//# sourceMappingURL=nest-kong.module.d.ts.map
