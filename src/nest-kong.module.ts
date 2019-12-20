29a424a3a52394dbb6808b9afdb09bff338f8ed6
Julien Sarazin <julien.sarazin@me.com>
2019-12-20 11:09:05 +0100

    updated initial configuration requirements

--- src/nest-kong.module.ts
+++ src/nest-kong.module.ts
@@ -10,36 +10,39 @@ import { ConfigService } from '@nestjs/config';
 export class NestKongModule {
 
     static init(options?: KongModuleConfiguration): DynamicModule {
-
-        const kongConfigurationProvider = {
-            provide: KONG_CONFIGURATION_PROVIDER,
-            useFactory: (configService: ConfigService): KongModuleConfiguration => configService.get<KongModuleConfiguration>('NKong', options),
-            inject: [ConfigService]
-        };
-
         /**
          * Configure kong client by connecting to the client agent
          * and register the current API if configuration is provided.
          */
         const kongClientProvider = {
             provide: KONG_CLIENT_PROVIDER,
-            useFactory: async (configuration: KongModuleConfiguration): Promise<any> => {
-                new Kong(options.kong)
-            },
-            inject: [KONG_CONFIGURATION_PROVIDER]
+            inject: [
+                ConfigService
+            ],
+            useFactory: async (configService: ConfigService): Promise<any> => {
+                const configuration = configService.get<KongModuleConfiguration>('NKong', options);
+                return new Kong(configuration.kong);
+            }
         };
 
         const kongServiceProvider = {
             provide: KONG_SERVICE_PROVIDER,
+            inject: [
+                KONG_CLIENT_PROVIDER,
+                KONG_CONFIGURATION_PROVIDER
+            ],
             useFactory: async (kongClient: Kong, kongConfiguration: KongModuleConfiguration): Promise<NestKongService> => {
                 return new NestKongService(kongClient, kongConfiguration);
             },
-            inject: [KONG_CLIENT_PROVIDER, KONG_CONFIGURATION_PROVIDER],
+
         };
 
         return {
             module: NestKongModule,
-            providers: [kongConfigurationProvider, kongClientProvider, kongServiceProvider],
+            imports: [
+                ConfigService,
+            ],
+            providers: [kongClientProvider, kongServiceProvider],
             exports: [kongServiceProvider],
         };
     }
